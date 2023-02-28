42c74f3482e096049eaefa49f36a9bdfcca85b5b
Ali <alidov363@gmail.com>
2023-02-28 12:04:01 +0400

    Add project files.

---
+++ ConfigurationApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace ConfigurationApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SocialMediaLinksOptions _options; //assigning the SocialMediaLinksOptions type
+
+        public HomeController(IOptions<SocialMediaLinksOptions> socialMediaLinks)
+        {
+            _options = socialMediaLinks.Value;
+        }
+
+        [Route("/")]    
+        public IActionResult Index()
+        {
+            ViewBag.Facebook = _options.Facebook;
+            ViewBag.Twitter = _options.Twitter;
+            ViewBag.Youtube = _options.Youtube;
+            ViewBag.Instagram = _options.Instagram;
+
+
+            return View();
+        }
+    }
+}
